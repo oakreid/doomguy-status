117b2a8d99c95a43b9b3a25cdbcbd3173640d5b4
oakreid <oakreid@gmail.com>
2020-07-05 04:46:37 +0000

    Add arms tab + ammo (timeleft) counter

--- render.py
+++ render.py
@@ -15,6 +15,7 @@ CENTER_Y = PIXEL_HEIGHT / 2
 INCHES_WIDTH = float(PIXEL_WIDTH) / DPI * 2
 INCHES_HEIGHT = float(PIXEL_HEIGHT) / DPI * 2
 REDNUM_Y = CENTER_Y - 5
+ARMSTAB_X = CENTER_X - 37
 ONES_BATTERY_X = CENTER_X - 82
 TENS_BATTERY_X = CENTER_X - 94
 HUNDRED_BATTERY_X = CENTER_X - 104
@@ -23,6 +24,9 @@ ONES_CPU_X = CENTER_X + 48
 TENS_CPU_X = CENTER_X + 36
 HUNDRED_CPU_X = CENTER_X + 26
 PERCENT_CPU_X = CENTER_X + 61
+HUNDREDS_TIMELEFT_X = CENTER_X - 150
+TENS_TIMELEFT_X = CENTER_X - 138
+ONES_TIMELEFT_X = CENTER_X - 126
 
 # load in required graphics
 def loadImage(path):
@@ -39,6 +43,7 @@ faceouch = loadImage("graphics/stfouch0.png")
 rednumbers = [loadImage(f"graphics/winum{n}.png") for n in range(10)]
 redpercent = loadImage("graphics/wipcnt.png")
 redminus = loadImage("graphics/sttminus.png")
+armstab = loadImage("graphics/starms.png")
 
 # formatting
 pyplot.rcParams["figure.dpi"] = DPI
@@ -57,18 +62,21 @@ while True:
     fig.set_size_inches(INCHES_WIDTH, INCHES_HEIGHT)
  
     # get relevant system info
-    battery_percent = sensors_battery()
+    battery_data = sensors_battery()
     cpu_usage_percent = int(cpu_percent())
 
     # rebuild image list
     images = [
-        (redpercent, PERCENT_BATTERY_X, REDNUM_Y),      # health percentage sign
-        (redpercent, PERCENT_CPU_X, REDNUM_Y),     # cpu usage percentage sign
+        (redpercent, PERCENT_BATTERY_X, REDNUM_Y),  # health percentage sign
+        (redpercent, PERCENT_CPU_X, REDNUM_Y),  # cpu usage percentage sign
+        (armstab, ARMSTAB_X, CENTER_Y)
     ]  # these are always in the same place
 
-    if battery_percent:
-        battery_percent = int(battery_percent[0])
+    if battery_data:
+        battery_percent = int(battery_data.percent)
+        battery_minsleft = int(battery_data.secsleft / 60)
         images.append((facelist[5 - ceil(battery_percent / 20)][randrange(3)], CENTER_X, CENTER_Y))  # face
+
         if battery_percent == 100:  # battery percent "health"
             images += [
                 (rednumbers[1], HUNDRED_BATTERY_X, REDNUM_Y),
@@ -82,11 +90,34 @@ while True:
             ]
         else:
             images.append((rednumbers[battery_percent], ONES_BATTERY_X, REDNUM_Y))
+
+        if battery_minsleft > 999:  # time left on charge "ammo" (minutes)
+            images += [
+                (rednumbers[9], HUNDREDS_TIMELEFT_X, REDNUM_Y),
+                (rednumbers[9], TENS_TIMELEFT_X, REDNUM_Y),
+                (rednumbers[9], ONES_TIMELEFT_X, REDNUM_Y),
+            ]
+        elif battery_minsleft >= 100:
+            images += [
+                (rednumbers[floor(battery_minsleft / 100)], HUNDREDS_TIMELEFT_X, REDNUM_Y),
+                (rednumbers[floor(battery_minsleft / 10)], TENS_TIMELEFT_X, REDNUM_Y),
+                (rednumbers[battery_minsleft % 10], ONES_TIMELEFT_X, REDNUM_Y),
+            ]
+        elif battery_percent > 9:
+            images += [
+                (rednumbers[floor(battery_minsleft / 10)], TENS_TIMELEFT_X, REDNUM_Y),
+                (rednumbers[battery_minsleft % 10], ONES_TIMELEFT_X, REDNUM_Y),
+            ]
+        else:
+            images.append((rednumbers[battery_minsleft], ONES_TIMELEFT_X, REDNUM_Y))
     else:
         images += [
             (faceouch, CENTER_X, CENTER_Y),
             (redminus, TENS_BATTERY_X, REDNUM_Y),
             (redminus, ONES_BATTERY_X, REDNUM_Y),
+            (redminus, HUNDREDS_TIMELEFT_X, REDNUM_Y),
+            (redminus, TENS_TIMELEFT_X, REDNUM_Y),
+            (redminus, ONES_TIMELEFT_X, REDNUM_Y),
         ]
    
     if cpu_usage_percent == 100:  # cpu usage percent "armor"
