0d5f41e61627a07f593867b0b989588959e9e250
Oakley Reid <oreid@starry.com>
2020-07-09 13:32:06 -0400

    Add daemon file for i3 status bar usage

---
+++ i3_daemon.py
@@ -0,0 +1,15 @@
+from i3ipc import Connection
+
+i3 = Connection()
+
+def float_status(i3, e):
+    status_bar = i3.get_tree().find_titled("Status Bar")
+    if status_bar:
+        status_bar[0].command("floating enable")
+        status_bar[0].command(f"move position 636 1000")
+        status_bar[0].command("sticky enable")
+        status_bar[0].command("border pixel 0")
+
+i3.on('window::new', float_status)
+
+i3.main()
