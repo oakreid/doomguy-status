7b987cdb32f22d85cf643d6cd7be86c5541e3671
Oakley Reid <oreid@starry.com>
2020-12-23 13:52:55 -0500

    Add pypoetry support

--- i3_daemon.py
+++ i3_daemon.py
@@ -6,7 +6,7 @@ def float_status(i3, e):
     status_bar = i3.get_tree().find_titled("Status Bar")
     if status_bar:
         status_bar[0].command("floating enable")
-        status_bar[0].command("move position 636 1000")
+        status_bar[0].command("move position 636 1020")
         status_bar[0].command("sticky enable")
         status_bar[0].command("border pixel 0")
 
